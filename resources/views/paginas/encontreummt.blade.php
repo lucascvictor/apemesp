b34e9217b2fc6ba89bfc2b8d9cacc01dae0c51c0
lukevictor <lucas.c.victor@hotmail.com>
2017-11-20 19:09:05 -0200

    Alterações na busca do MT

--- resources/views/paginas/encontreummt.blade.php
+++ resources/views/paginas/encontreummt.blade.php
@@ -10,9 +10,6 @@
        <div class="row">
          {{ Form::open(['action' => ['Apemesp\EncontreUmMtController@search'], 'method' => 'GET']) }}
 
-
-
-
             <!--Filtro por especialidade -->
               <div class="form-group col-xs-8 col-md-6">
                             <label>Filtre por especialidade:</label>
@@ -50,59 +47,62 @@
                              <p class="help-block text-danger"></p>
                </div>
                <!-- Botão de busca por nome-->
-                <div class="col-md-10" style="margin-top: 30px;">
-                    <div class="form-group input-group">
-                         <input id="nome" class="nome" type="text" class="form-control" placeholder="Pesquisa por nome" width="100%">
-                         <span class="input-group-btn"></span>
-                     </div>
+               <div class="form-group col-xs-8 col-md-6">
+                          <label>Filtre por Nome:</label>
+                         <input id="nome" class="nome form-control" type="text" placeholder="Pesquisa por nome" width="100%">
+
                  </div>
 
-               <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
+
                </div>
+
+<button class="btn btn-default" type="submit"><i class="fa fa-search"></i>Pesquisar</button>
+
 
            {{ Form::close() }}
 
+           <div class="row">
+                   <div class="col-lg-12 col-md-10">
+
+
+                       <div class="table-responsive">
+                           <table class="table table-hover table-striped">
+                               <thead>
+                                   <tr>
+                                       <th>Nome do musicoterapeuta</th>
+                                       <th>Local de atendimento</th>
+                                       <th>Telefone de contato</th>
+                                       <th>Dias de atendimento </th>
+                                       <th>Envie uma mensagem</th>
+                                   </tr>
+                               </thead>
+                               <tbody>
+
+                                      <tr>
+                                         @if($pesquisa == 0)
+                                         <td></td>
+                                         <td></td>
+                                         <td></td>
+                                         <td></td>
+                                         <td></td>
+                                         @else
+
+                                         <td><a class="btn btn-primary">Enviar e-mail</a></td>
+                                         @endif
+                                       </tr>
+
+
+
+
+                               </tbody>
+                           </table>
+                       </div>
+                   </div>
+              </div>
+
        </div>
 
-            <div class="row">
-                    <div class="col-lg-12 col-md-10">
 
-
-                        <div class="table-responsive">
-                            <table class="table table-hover table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Nome do musicoterapeuta</th>
-                                        <th>Local de atendimento</th>
-                                        <th>Telefone de contato</th>
-                                        <th>Dias de atendimento </th>
-                                        <th>Envie uma mensagem</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-								                        <tr>
-                                          @if($pesquisa == 0)
-                                          <td></td>
-                                          <td></td>
-                                          <td></td>
-                                          <td></td>
-                                          <td></td>
-                                          @else
-
-                                          <td><a class="btn btn-primary">Enviar e-mail</a></td>
-                                          @endif
-                                        </tr>
-
-
-
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-
-	</div>
 
 
 
