5a84b0c5fc3eeedbcadbd586c995f67f66cd48b6
lukevictor <lucas.c.victor@hotmail.com>
2018-02-03 18:37:11 -0200

    Criacao do botao para upload do estatuto em PDF

--- resources/views/admin/admin/paginas/editarpagina.blade.php
+++ resources/views/admin/admin/paginas/editarpagina.blade.php
@@ -10,19 +10,36 @@
 
 @section('conteudo')
 
+            <!-- Botão de upload do arquivo PDF do estatuto-->
+            @if($pagina->id == 2)
+             <form method="POST"  enctype="multipart/form-data" action="{{ url('/apemesp/estatuto/pdf') }}">
+                {{ csrf_field() }}
+                <fieldset>
+                <legend>Upload do estatuto em PDF</legend>
+                
+                    <input id="estatuto" name="estatuto" class="input-file" type="file" required>
+                    <br>
+                    <div class="col-md-4">
+                    <button type="submit" class="btn btn-success"> Upload</button>
+                    </div>
+                </fieldset>
+                
+                </form>
+                <hr>
+            @endif
+
             {!! Form::open(array('route' => ['pagina.update', $pagina->id], 'data-parsley-validate' => '')) !!}
 
-    			{{ Form::label('titulo', 'Titulo:') }}
-                {{ Form::text('titulo', $pagina->titulo ,array('class' => 'form-control')) }}
+            {{ Form::label('titulo', 'Titulo:') }}
+            {{ Form::text('titulo', $pagina->titulo ,array('class' => 'form-control')) }}
 
-          {{ Form::label('subtitulo', 'Subtitulo:') }}
-                {{ Form::text('subtitulo', $pagina->subtitulo ,array('class' => 'form-control')) }}
+            {{ Form::label('subtitulo', 'Subtitulo:') }}
+            {{ Form::text('subtitulo', $pagina->subtitulo ,array('class' => 'form-control')) }}
 
+            {{ Form::label('conteudo', 'Conteudo:') }}
+            {{ Form::textarea('conteudo', $pagina->body, array('class' => 'form-control')) }}
 
-    			{{ Form::label('conteudo', 'Conteudo:') }}
-    			{{ Form::textarea('conteudo', $pagina->body, array('class' => 'form-control')) }}
-
-    			{{ Form::submit('Atualizar', array('class' => 'btn btn-success btn-lg btn-block')) }}
+            {{ Form::submit('Atualizar', array('class' => 'btn btn-success btn-lg btn-block')) }}
 
 			{!! Form::close() !!}
 
