e5d33672322d6b3c0abbad591475135e3860e017
lukevictor <lucas.c.victor@hotmail.com>
2018-02-25 11:13:21 -0300

    Bloqueio de envio vazio na pagina de contato

--- resources/views/jomesp/contato.blade.php
+++ resources/views/jomesp/contato.blade.php
@@ -25,20 +25,20 @@
                         <div class="row">
                             <div class="form-group col-lg-4">
                                 <label>Nome</label>
-                                <input type="text" name="nome" class="form-control">
+                                <input type="text" name="nome" class="form-control" required="">
                             </div>
                             <div class="form-group col-lg-4">
                                 <label>Endereço de e-mail</label>
-                                <input type="email" name="email" class="form-control">
+                                <input type="email" name="email" class="form-control" required="">
                             </div>
                             <div class="form-group col-lg-4">
                                 <label>Telefone</label>
-                                <input type="tel" name="telefone" class="form-control">
+                                <input type="tel" name="telefone" class="form-control" required="">
                             </div>
                             <div class="clearfix"></div>
                             <div class="form-group col-lg-12">
                                 <label>Mensagem</label>
-                                <textarea class="form-control" name="mensagem" rows="6"></textarea>
+                                <textarea class="form-control" name="mensagem" rows="6" required=""></textarea>
                             </div>
                             <div class="form-group col-lg-12">
                                 <input type="hidden" name="assunto" value="jomesp">
