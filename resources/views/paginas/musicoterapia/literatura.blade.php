6eec0b8681a65112c59f1597d7bf4f7dec5ac29c
lukevictor <lucas.c.victor@hotmail.com>
2018-03-18 18:28:09 -0300

    Controle de indicações literarias

--- resources/views/paginas/musicoterapia/literatura.blade.php
+++ resources/views/paginas/musicoterapia/literatura.blade.php
@@ -1,33 +1,22 @@
 @extends('main')
-
 @section('titulo', 'Literatura')
-
 @section('conteudo')
-
 <div class="row">
-<a href="{{ url('/musicoterapia/literatura/indicacao') }}">
-{{ Form::button('Indique um item para o acervo', array('class' => 'btn btn-success btn-md btn-block')) }}
-</a>
+   <a href="{{ url('/musicoterapia/literatura/indicacao') }}">
+   {{ Form::button('Indique um item para o acervo', array('class' => 'btn btn-success btn-md btn-block')) }}
+   </a>
 </div>
 <div class="row">
-	@foreach($literaturas as $literatura)
-
-	<div class="col-md-6 col-lg-6">
-
-		<!-- Adicionar botão para indicar Livros -->
-
-		<p><h2>{{ $literatura->titulo }}</h2></p>
-
-		<img alt="{{ $literatura->titulo }}" src="{{ url('/images/musicoterapia/literatura')}}/{{ $literatura->imagem }}" style="height:200px; width:280px" title="Revista Brasileira de Mt" />
-
-		{!! $literatura->conteudo !!}
-
-
-
-	</div>
-	@endforeach
-	<p> {{ $literaturas->render() }} </p>
-
-	</div>
-
+   @foreach($literaturas as $literatura)
+   <div class="col-md-4 col-lg-4">
+      <!-- Adicionar botão para indicar Livros -->
+      <p>
+      <h2>{{ $literatura->titulo }}</h2>
+      </p>
+      <img alt="{{ $literatura->titulo }}" src="{{ url('/images/musicoterapia/literatura')}}/{{ $literatura->imagem }}" style="height:30%; width:80%;" title="Revista Brasileira de Mt" />
+      {!! $literatura->conteudo !!}
+   </div>
+   @endforeach
+   <p> {{ $literaturas->render() }} </p>
+</div>
 @endsection
