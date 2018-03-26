04f9a8eaa931958ccbea07f4edf6f56e80b4ef08
lukevictor <lucas.c.victor@hotmail.com>
2018-03-25 22:43:05 -0300

    correção de paginacao em producao

--- resources/views/admin/admin/usuarios/index.blade.php
+++ resources/views/admin/admin/usuarios/index.blade.php
@@ -74,7 +74,7 @@
          </div>
       </div>
       <div class="row">
-         <p> {!! $usuarios->appends(request()->query())->links() !!} </p>
+         <p> {!! $usuarios->render() !!} </p>
       </div>
    </div>
    <div class="col-md-2">
