46e698173795785f6e92ad92626f2d952c84c530
Lucas <lucas.c.victor@hotmail.com>
2018-03-29 12:43:41 -0300

    Correção de direcionamento

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/public'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
