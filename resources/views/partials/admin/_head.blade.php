6660fbf0e8300386d4236eee0e62b2a564a821c9
lukevictor <lucas.c.victor@hotmail.com>
2018-03-31 15:01:55 -0300

    correções de estilização

--- resources/views/partials/admin/_head.blade.php
+++ resources/views/partials/admin/_head.blade.php
@@ -4,19 +4,20 @@
     <!-- Bootstrap Core CSS -->
     {!! Html::style('css/bootstrap.min.css') !!}
 
-    <!-- Custom CSS -->
-    {!! Html::style('css/sb-admin.css') !!}
+    <link media="all" type="text/css" rel="stylesheet" href="{{ url('css/sb-admin.css') }}">
 
     <!-- Custom CSS -->
-    {!! Html::style('css/dataTables.bootstrap4.css.css') !!}
+    {!! Html::style('public/css/dataTables.bootstrap4.css') !!}
 
 
     <!-- Morris Charts CSS -->
-    {!! Html::style('css/plugins/morris.css') !!}
+    {!! Html::style('public/css/plugins/morris.css') !!}
 
 
-    <!-- Custom Fonts -->
-    {!! Html::style('font-awesome/css/font-awesome.min.css') !!}
+    <link media="all" type="text/css" rel="stylesheet" href="{{ url('font-awesome/css/font-awesome.min.css') }}">
 
 
-   
+
+
+    
+
