721e089db5a4ef8f26029bbc25bb58c624204ed0
lukevictor <lucas.c.victor@hotmail.com>
2018-04-06 12:02:44 -0300

    Correção dos icones da area publica

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -2,7 +2,7 @@
 <link rel="icon" type="imagem/jpg" href="http://www.apemesp.com/public/images/posts/imagens/previas/apemesp.jpg" />
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link media="all" type="text/css" rel="stylesheet" href="http://www.apemesp.com/public/font-awesome/css/font-awesome.min.css">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <link media="all" type="text/css" rel="stylesheet" href="http://www.apemesp.com/public/css/bootstrap-social.css">
 <style>
    .btn{
