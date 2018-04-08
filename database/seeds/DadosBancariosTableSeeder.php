8a7573de7bae598f0ce7d030ab2b93a3536f16b1
lukevictor <lucas.c.victor@hotmail.com>
2018-04-08 18:51:20 -0300

    Correção do seed de dados bancarios

--- database/seeds/DadosBancariosTableSeeder.php
+++ database/seeds/DadosBancariosTableSeeder.php
@@ -11,18 +11,18 @@ class DadosBancariosTableSeeder extends Seeder
      */
     public function run()
     {
-       DB::table('dados_bancarios')->delete();
-       DB::table("dados_bancarios")->insert([
-        [
-            "id"      => 1,
-            "titular"   => "APEMESP, Assoc de Profissionais e Estudantes de Musicoterapia do Estado de São Paulo",
-            "documento" => '64016934/0001-02',
-            "banco" => 'Bradesco (237)',
-            "agencia" => '0478',
-            "conta" => '61035-6',
-            "created_at" =>'2018-03-30 14:27:56 ',
-            "updated_at" =>'2018-03-30 14:27:56 ',
-        ],
-       ]);
+        DB::table('dados_bancarios')->delete();
+        DB::table("dados_bancarios")->insert([
+         [
+             "id"      => 1,
+             "titular"   => "APEMESP, Assoc de Profissionais e Estudantes de Musicoterapia do Estado de São Paulo",
+             "documento" => '64016934/0001-02',
+             "banco" => 'Bradesco (237)',
+             "agencia" => '0478',
+             "conta" => '61035-6',
+             "created_at" =>'2018-03-30 14:27:56 ',
+             "updated_at" =>'2018-03-30 14:27:56 ',
+         ],
+        ]);
     }
 }
